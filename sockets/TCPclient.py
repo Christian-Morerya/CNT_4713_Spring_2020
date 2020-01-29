b482b36d4c91f3bce07fcbce466e132303f4984e
Christian Moreyra <cmore100@fiu.edu>
2020-01-29 13:50:44 -0500

    Completed TCP copy upload

--- sockets/TCPclient.py
+++ sockets/TCPclient.py
@@ -3,10 +3,10 @@
 from socket import socket, AF_INET, SOCK_STREAM
 serverName = 'localhost'
 serverPort = 12001
-clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket = socket (AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 message = raw_input('Input lowercase sentence: ')
 clientSocket.send(message)
 modifiedMessage = clientSocket.recv(2048)
-print 'From Server: ', modifiedMessage
+print('From Server: ', modifiedMessage)
 clientSocket.close()
